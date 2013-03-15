2933712cfa3f0624ce23cb15d09ec78ffbaa70c0
atb@isep.ipp.pt <>
2013-03-15 17:47:29 +0000

    actualizacoes de uns scripts do windows relativos a geracao de documentacao

---
+++ scripts/makeuml.bat
@@ -0,0 +1,5 @@
+REM Vai gerar as imagens relativas aos diagramas de UML em pastas com o nome doc-files
+REM O javadoc copia automaticamente essas pastas para a pasta com o resultado do java-doc
+java -jar ../lib/plantuml.jar "../src*/**.java"
+java -jar ../lib/plantuml.jar "../doc/javadoc-overview.html"
+
