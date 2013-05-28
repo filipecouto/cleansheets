bfa271a3c241723d940dba28d5c11dd1e1a744a9
RitaNogueira_1110494 <1110494@isep.ipp.pt>
2013-05-28 18:56:28 +0100

    Update method write(...)

--- src/csheets/io/XMLCodec.java
+++ src/csheets/io/XMLCodec.java
@@ -4,106 +4,215 @@
  */
 package csheets.io;
 
-import csheets.core.Spreadsheet;
-import csheets.core.Workbook;
-import csheets.ext.style.StylableCell;
-import csheets.ext.style.StyleExtension;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import javax.swing.text.Element;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import csheets.core.Spreadsheet;
+import csheets.core.Workbook;
+import csheets.ext.style.StylableCell;
+import csheets.ext.style.StyleExtension;
 
 /**
  * A codec for xml files.
+ * 
  * @author Filipe Silva & Rita Nogueira
  */
-public class XMLCodec implements Codec{
+public class XMLCodec implements Codec {
 
     @Override
-    public Workbook read(InputStream stream) throws IOException, ClassNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Workbook read(InputStream stream) throws IOException,
+	    ClassNotFoundException {
+	throw new UnsupportedOperationException("Not supported yet."); // To
+								       // change
+								       // body
+								       // of
+								       // generated
+								       // methods,
+								       // choose
+								       // Tools
+								       // |
+								       // Templates.
     }
 
     @Override
-    public void write(Workbook workbook, OutputStream stream) throws IOException {
-        System.out.println("Writing!");
-        // Wraps stream
-        PrintWriter writer = new PrintWriter(new BufferedWriter(
-                new OutputStreamWriter(stream)));
+    public void write(Workbook workbook, OutputStream stream)
+	    throws IOException {
+	System.out.println("Writing!");
+	// Wraps stream
+	// Writes content of rows
+	try {
 
-        // Writes content of rows
-        Spreadsheet sheet = workbook.getSpreadsheet(0);
-        int textAlign, cellAlign;
-        String textAlignString, cellAlignString, styleBold="False",styleItalic="False";
-        for (int row = 0; row < sheet.getRowCount(); row++) {
-                for (int column = 0; column < sheet.getColumnCount(); column++)
-                        if (column + 1 < sheet.getColumnCount()){
-                            StylableCell stylableCell = (StylableCell)sheet.getCell(column, row).getExtension(StyleExtension.NAME);
-                            textAlign=stylableCell.getHorizontalAlignment();
-                            switch(textAlign){
-                                case 0: textAlignString="Center"; break;
-                                case 2: textAlignString="Left"; break;
-                                case 4: textAlignString="Right"; break;
-                                default: textAlignString="Left"; break;
-                            }
-                            cellAlign=stylableCell.getVerticalAlignment();
-                            switch(cellAlign){
-                                case 0: cellAlignString="Center"; break;
-                                case 1: cellAlignString="Top"; break;
-                                case 3: cellAlignString="Bottom"; break;
-                                default: cellAlignString="Center"; break;
-                            }
-                            if(stylableCell.getFont().isPlain()){
-                                styleBold="False";
-                                styleItalic="False";
-                            }
-                            else if(stylableCell.getFont().isBold() && stylableCell.getFont().isItalic()){
-                                styleBold="True";
-                                styleItalic="True";
-                            }
-                            else if(stylableCell.getFont().isBold()){
-                                styleBold="True";
-                                styleItalic="False";
-                            }
-                            else if(stylableCell.getFont().isItalic()){
-                                styleBold="False";
-                                styleItalic="True";
-                            }
-                            writer.print("<Cell Column=" + column
-                            + "Row=" + row
-                            + "BackgroundColor=" + stylableCell.getBackgroundColor().getRGB()
-                            + "BorderTop=" + stylableCell.getBorder().toString()
-                            + "BorderBottom=" + "BA"
-                            + "BorderLeft=" + "BA"
-                            + "BorderRight=" + "BA"
-                            + "BorderColor=" + "BA"
-                            + "TextAlign=" + textAlignString
-                            + "CellAlign=" + cellAlignString
-                            + "ForegroundColor=" + stylableCell.getForegroundColor().getRGB()
-                            + "FontFamily=" + stylableCell.getFont().getFamily()
-                            + "FontSize=" + stylableCell.getFont().getSize()
-                            + "Bold=" + styleBold
-                            + "Italic=" + styleItalic
-                            + "Underline=" + "False" 
-                            + ">" + sheet.getCell(column, row).getContent());
-                        }
-                if (row + 1 < sheet.getRowCount())
-                        writer.println();
-        }
+	    DocumentBuilderFactory docFactory = DocumentBuilderFactory
+		    .newInstance();
+	    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+	    // root elements
+	    Document doc = docBuilder.newDocument();
+	    Element workBook = (Element) doc.createElement("Workbook");
+	    // set attribute to spreadSheet element
+	    Attr attr = doc.createAttribute("name");
+	    attr.setValue("Workbook Name");
+	    workBook.setAttributeNode(attr);
+	    doc.appendChild(workBook);
+	    for (Spreadsheet sheet : workbook) {
+		// spreadSheet elements
+		Element spreadSheet = doc.createElement("SpreadSheet");
+		workBook.appendChild(spreadSheet);
+		attr = doc.createAttribute("name");
+		attr.setValue("SpreadSheet Name");
+		spreadSheet.setAttributeNode(attr);
 
+		int textAlign, cellAlign;
+		String textAlignString, cellAlignString, styleBold = "False", styleItalic = "False";
+		for (int row = 0; row < sheet.getRowCount(); row++) {
+		    for (int column = 0; column < sheet.getColumnCount(); column++)
+			if (column + 1 < sheet.getColumnCount()) {
+			    StylableCell stylableCell = (StylableCell) sheet
+				    .getCell(column, row).getExtension(
+					    StyleExtension.NAME);
+			    textAlign = stylableCell.getHorizontalAlignment();
+			    switch (textAlign) {
+			    case 0:
+				textAlignString = "Center";
+				break;
+			    case 2:
+				textAlignString = "Left";
+				break;
+			    case 4:
+				textAlignString = "Right";
+				break;
+			    default:
+				textAlignString = "Left";
+				break;
+			    }
+			    cellAlign = stylableCell.getVerticalAlignment();
+			    switch (cellAlign) {
+			    case 0:
+				cellAlignString = "Center";
+				break;
+			    case 1:
+				cellAlignString = "Top";
+				break;
+			    case 3:
+				cellAlignString = "Bottom";
+				break;
+			    default:
+				cellAlignString = "Center";
+				break;
+			    }
+			    if (stylableCell.getFont().isPlain()) {
+				styleBold = "False";
+				styleItalic = "False";
+			    } else if (stylableCell.getFont().isBold()
+				    && stylableCell.getFont().isItalic()) {
+				styleBold = "True";
+				styleItalic = "True";
+			    } else if (stylableCell.getFont().isBold()) {
+				styleBold = "True";
+				styleItalic = "False";
+			    } else if (stylableCell.getFont().isItalic()) {
+				styleBold = "False";
+				styleItalic = "True";
+			    }
 
-        // Frees resources
-        writer.close();
-        stream.close();
-        System.out.println("Done!");
+			    // cell elements
+			    Element cell = doc.createElement("Cell");
+			    spreadSheet.appendChild(cell);
+			    attr = doc.createAttribute("Column");
+			    attr.setValue("" + column);
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("Row");
+			    attr.setValue("" + row);
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("BackgroundColor");
+			    attr.setValue(""
+				    + stylableCell.getBackgroundColor()
+					    .getRGB());
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("BorderTop");
+			    attr.setValue("False");
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("BorderBottom");
+			    attr.setValue("False");
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("BorderLeft");
+			    attr.setValue("False");
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("BorderRight");
+			    attr.setValue("False");
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("BorderColor");
+			    attr.setValue("000000");
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("TextAlign");
+			    attr.setValue(textAlignString);
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("CellAlign");
+			    attr.setValue(cellAlignString);
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("ForegroundColor");
+			    attr.setValue(""
+				    + stylableCell.getForegroundColor()
+					    .getRGB());
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("FontFamily");
+			    attr.setValue(""
+				    + stylableCell.getFont().getFamily());
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("FontSize");
+			    attr.setValue("" + stylableCell.getFont().getSize());
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("Bold");
+			    attr.setValue(styleBold);
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("Italic");
+			    attr.setValue(styleItalic);
+			    cell.setAttributeNode(attr);
+			    attr = doc.createAttribute("Underline");
+			    attr.setValue("False");
+			    cell.setAttributeNode(attr);
+
+			    TransformerFactory transformerFactory = TransformerFactory
+				    .newInstance();
+			    Transformer transformer = transformerFactory
+				    .newTransformer();
+			    DOMSource source = new DOMSource(doc);
+			    StreamResult result = new StreamResult(stream);
+
+			    // Output to console for testing
+			    // StreamResult result = new
+			    // StreamResult(System.out);
+
+			    transformer.transform(source, result);
+
+			}
+		}
+	    }
+	} catch (ParserConfigurationException pce) {
+	    pce.printStackTrace();
+	} catch (TransformerException tfe) {
+	    tfe.printStackTrace();
+	}
+
+	// Frees resources
+	stream.close();
+	System.out.println("Done!");
     }
-    
 }
