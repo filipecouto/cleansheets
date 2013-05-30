1e1186034fb037e1ffa8c1fa699f5de31c868afe
Gil Castro <1110484@isep.ipp.pt>
2013-05-30 21:12:49 +0100

    Small fixes and comments in order to help Filipe Couto develop the GUI

--- src/csheets/ext/rtc/ui/ConnectionWindow.java
+++ src/csheets/ext/rtc/ui/ConnectionWindow.java
@@ -13,6 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+// REFACTOR: this name doesn't make much sense either
 public class ConnectionWindow extends JDialog {
 
     private JTextField ipAddress;
@@ -22,21 +23,20 @@ public class ConnectionWindow extends JDialog {
     private JPanel panelButtons;
 
     public ConnectionWindow(RtcSidebar sidebar) {
-	super((JFrame) null, "Connection", true);
+	super((JFrame) null, "Connection (this title doesn't make much sense)",
+		true);
 
-	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-	JPanel panel = new JPanel();
-	panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+	// JPanel panel = new JPanel();
+	// panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 
 	// creates textfield for insertion
 	ipAddress = new JTextField();
-	ipAddress.setText("INSERT IP ADDRESS OR URL");
+	ipAddress.setText("INSERT IP ADDRESS OR URL"); // YEAH LET'S YELL!
 	ipAddress.addFocusListener(new FocusListener() {
 	    @Override
 	    public void focusLost(FocusEvent arg0) {
-		// TODO Auto-generated method stub
-
 	    }
 
 	    @Override
@@ -44,33 +44,34 @@ public class ConnectionWindow extends JDialog {
 		ipAddress.selectAll();
 	    }
 	});
-	
+
 	buttonAccept = new JButton("Accept");
 	buttonAccept.addActionListener(new ActionListener() {
-	    
 	    @Override
 	    public void actionPerformed(ActionEvent arg0) {
 		listener.onIPSelected(ipAddress.getText());
 	    }
 	});
-	
+
 	buttonCancel = new JButton("Cancel");
 	buttonCancel.addActionListener(new ActionListener() {
-	    
 	    @Override
 	    public void actionPerformed(ActionEvent arg0) {
-		listener.onIPSelected("CANCELED");
+		listener.onIPSelected("CANCELED");// YEAH LET'S YELL AND SEND A
+						  // FALSE ALARM TO THE OWNER OF
+						  // THIS DIALOG!
 	    }
 	});
-	panel.add(ipAddress);
+	add(ipAddress);
 	panelButtons = new JPanel();
 	panelButtons.setLayout(new BoxLayout(panelButtons, BoxLayout.X_AXIS));
 	panelButtons.add(buttonAccept);
 	panelButtons.add(buttonCancel);
-	panel.add(panelButtons);
-	setVisible(true);
+	add(panelButtons);
+
+	pack();
     }
-    
+
     public void setOnIpSelectedListener(OnIPSelectListener listener) {
 	this.listener = listener;
     }
