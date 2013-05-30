79d2b4bc92016a076a60d0cc5c0740c55a5fcee6
Gil Castro <1110484@isep.ipp.pt>
2013-05-30 16:39:34 +0100

    Improvements on class architecture (see #4)

---
+++ src/csheets/ext/rtc/MessageTypes.java
@@ -0,0 +1,5 @@
+package csheets.ext.rtc;
+
+public enum MessageTypes {
+    disconnect, info, infoList, eventCellSelected, eventCellChanged, getCells, workbook, getSpreadsheet, spreadsheet, cells
+}
