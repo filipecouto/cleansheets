170dc76b710fdab81ee47e6f2b710c486a00bba4
Filipe Silva 1110687 <>
2013-05-30 16:22:14 +0100

    1110687 e 1110484: creation of class XMLValidator and method validate ref #2

---
+++ src/csheets/io/XMLValidator.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.io;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.*;
+import org.xml.sax.SAXException;
+import java.io.*;
+
+/**
+ *
+ * @author Filipe Silva & Rita Nogueira
+ */
+public class XMLValidator {
+    
+    /**
+     * Creates a new XML validator.
+     */
+    public XMLValidator(){
+    }
+    
+    /*
+     * 
+     */
+    public void validate(InputStream stream) throws Exception{
+        Source schemaFile = new StreamSource(new File("src/csheets/io/XMLSchema.xsd"));
+        Source xmlFile = new StreamSource(stream);
+
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(schemaFile);
+        Validator validator = schema.newValidator();
+        
+        try{
+            validator.validate(xmlFile);
+            System.out.println(xmlFile.getSystemId() + " is valid");
+        }
+        catch (SAXException e) 
+        {
+            System.out.println(xmlFile.getSystemId() + " is NOT valid");
+            System.out.println("Reason: " + e.getLocalizedMessage());
+        }
+    }
+}
