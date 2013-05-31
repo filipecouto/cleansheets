bcf79d18fe60067d73d0eed6632b4b7885abee40
noobshark <FILIPE@FILIPE-PC>
2013-05-31 16:13:28 +0100

    Share Action updated see #4

--- src/csheets/ext/rtc/ui/ShareAction.java
+++ src/csheets/ext/rtc/ui/ShareAction.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import csheets.core.Address;
 import csheets.ext.rtc.ClientInfo;
 import csheets.ext.rtc.RealTimeCollaboration;
+import csheets.ext.rtc.RtcShareProperties;
 import csheets.ui.ctrl.FocusOwnerAction;
 import csheets.ui.ctrl.UIController;
 
@@ -15,7 +16,7 @@ public class ShareAction extends FocusOwnerAction {
     private UIController uiController;
     private DataListener dataListener;
     private ShareOptionsDialog optDialog;
-    
+
     public ShareAction(RealTimeCollaboration extension,
 	    UIController uiController) {
 	this.extension = extension;
@@ -25,39 +26,39 @@ public class ShareAction extends FocusOwnerAction {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-	if(optDialog == null) {
-        	optDialog = new ShareOptionsDialog();
-        	optDialog.setOnChooseExportListener(new OnChooseExportListener() {
-        
-        	    @Override
-        	    public void onChoosedExport(boolean export) {
-        		// selected cells if false , whole spreadsheet if true
-        		Address cells[] = new Address[2];
-        		Address cell1;
-        		Address cell2;
-        		if (export) {
-        		    cell1 = new Address(1, 1);
-        		    cell2 = new Address(1, 1);
-        		} else {
-        		    cell1 = new Address(1, 1);
-        		    cell2 = new Address(1, 1);
-        		}
-        		cells[0] = cell1;
-        		cells[1] = cell2;
-        		String ip = "";
-        		ClientInfo server = null;
-        		try {
-        		    server = extension.createServer(new ClientInfo("Servidor"),
-        			    uiController, cells);
-        		    ip = server.getAddress().toString();
-        		} catch (IOException e1) {
-        		    e1.printStackTrace();
-        		} finally {
-        		    dataListener.onSendData(server, ip);
-        		}
-        	    }
-        	});
-        	optDialog.setVisible(true);
+	if (optDialog == null) {
+	    optDialog = new ShareOptionsDialog();
+	    optDialog.setOnChooseExportListener(new OnChooseExportListener() {
+
+		@Override
+		public void onChoosedExport(boolean export) {
+		    // selected cells if false , whole spreadsheet if true
+		    Address cell1;
+		    Address cell2;
+		    if (export) {
+			cell1 = new Address(1, 1);
+			cell2 = new Address(1, 1);
+		    } else {
+			cell1 = new Address(1, 1);
+			cell2 = new Address(1, 1);
+		    }
+		    String ip = "";
+		    ClientInfo server = null;
+		    try {
+			RtcShareProperties props = new RtcShareProperties();
+			props.setRange(cell1, cell2);
+			props.setSpreadsheet(0);
+			server = extension.createServer(new ClientInfo(
+				"Servidor"), props, uiController);
+			ip = server.getAddress().toString();
+		    } catch (IOException e1) {
+			e1.printStackTrace();
+		    } finally {
+			dataListener.onSendData(server, ip);
+		    }
+		}
+	    });
+	    optDialog.setVisible(true);
 	} else {
 	    optDialog.setVisible(true);
 	}
