8c4771d2bc8b2629d4870d1b9e946e2356ab4773
noobshark <FILIPE@FILIPE-PC>
2013-05-31 14:34:16 +0100

    Action is now passed by parameter and associated to the button. see #4

--- src/csheets/ext/rtc/ui/RtcSidebar.java
+++ src/csheets/ext/rtc/ui/RtcSidebar.java
@@ -32,89 +32,21 @@ public class RtcSidebar extends JPanel {
     private ClientsListAdapter adapter;
 
     public RtcSidebar(final RealTimeCollaboration extension,
-	    final UIController uiController) {
+	    final UIController uiController, ShareAction shareAction, ConnectAction connectAction) {
 	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 	JPanel buttonPanel = new JPanel();
 	buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
 	
-	bShare = new JButton("Share");
+	bShare = new JButton(shareAction);
 	bShare.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 	buttonPanel.add(bShare);
-	bShare.addActionListener(new ActionListener() {
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-		
-		ShareOptionsDialog optDialog = new ShareOptionsDialog();
-		optDialog.setOnChooseExportListener(new onChooseExportListener() {
-		    
-		    @Override
-		    public void onChoosedExport(boolean export) {
-			// selected cells if false , whole spreadsheet if true
-			Address cells[] = new Address[2];
-			Address cell1;
-			Address cell2;
-			if(export) {
-			    cell1 = new Address(1,1);
-			    cell2 = new Address(1,1);
-			} else {
-			    cell1 = new Address(1,1);
-			    cell2 = new Address(1,1);
-			}
-			cells[0] = cell1;
-			cells[1] = cell2;
-			String ip = "";
-			try {
-			    ip = extension
-				    .createServer(new ClientInfo("Servidor"),
-					    uiController, cells).getAddress().toString();
-			} catch (IOException e1) {
-			    e1.printStackTrace();
-			} finally {
-			    ipAddress.setText("Your ip address: " + ip);
-			    bShare.setVisible(false);
-			    bConnect.setVisible(false);
-			    bDisconnect.setVisible(true);
-			}
-		    }
-		});
-		/*String ip = "";
-		*/
-		optDialog.setVisible(true);
-	    }
-	});
-	bConnect = new JButton("Connect");
+	bConnect = new JButton(connectAction);
 	bConnect.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 	ipAddress = new JLabel();
 	ipAddress.setText("Not connected");
 	ipAddress.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 	buttonPanel.add(bConnect);
-	// add(ipAddress);
-	bConnect.addActionListener(new ActionListener() {
-	    @Override
-	    public void actionPerformed(ActionEvent e) {
-
-		ConnectionWindow ipDialog = new ConnectionWindow();
-		ipDialog.setOnIpSelectedListener(new OnIPSelectListener() {
-		    @Override
-		    public void onIPSelected(String address, String username) {
-			try {
-			    extension.createClient(new ClientInfo(username),
-				    address, uiController);
-			} catch (UnknownHostException e1) {
-			    e1.printStackTrace();
-			} catch (IOException e1) {
-			    e1.printStackTrace();
-			} finally {
-			    ipAddress.setText("Your ip address: " + address);
-			    bShare.setVisible(false);
-			    bConnect.setVisible(false);
-			    bDisconnect.setVisible(true);
-			}
-		    }
-		});
-		ipDialog.setVisible(true);
-	    }
-	});
+	
 	bDisconnect = new JButton("Disconnect");
 	bDisconnect.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 	bDisconnect.addActionListener(new ActionListener() {
