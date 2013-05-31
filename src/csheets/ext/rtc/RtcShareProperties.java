6230f02e5f6b3ce44cda50f4edcf632df107ee56
Gil Castro <1110484@isep.ipp.pt>
2013-05-31 15:06:51 +0100

    RtcShareProperties - class that tells what is shared and checks for unauthorized accesses (see #4)

---
+++ src/csheets/ext/rtc/RtcShareProperties.java
@@ -0,0 +1,53 @@
+package csheets.ext.rtc;
+
+import csheets.core.Address;
+import csheets.ext.rtc.messages.RemoteCell;
+
+public class RtcShareProperties {
+    private int spreadsheet;
+
+    private Address start;
+    private Address end;
+
+    public RtcShareProperties() {
+	spreadsheet = 0;
+	start = new Address(1, 1);
+	end = new Address(5, 8);
+    }
+
+    public void setRange(Address start, Address end) {
+	this.start = start;
+	this.end = end;
+    }
+
+    public void setSpreadsheet(int spreadsheet) {
+	this.spreadsheet = spreadsheet;
+    }
+    
+    public int getSpreadsheet() {
+	return spreadsheet;
+    }
+
+    /**
+     * Tells whether address is inside the specified range
+     * 
+     * @param address
+     * @return true if it is
+     */
+    public boolean isInsideRange(Address address) {
+	return start.getColumn() >= address.getColumn()
+		&& start.getRow() >= start.getRow()
+		&& end.getColumn() <= address.getColumn()
+		&& end.getRow() <= address.getRow();
+    }
+
+    /**
+     * Tells whether this remote change is permitted by current share properties
+     * 
+     * @param cell
+     * @return
+     */
+    public boolean isValid(RemoteCell cell) {
+	return true;
+    }
+}
