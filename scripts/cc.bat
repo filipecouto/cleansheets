ecd447aeebeb651ca63b83956ed916c857e683e1
Filipe Silva <1110687@isep.ipp.pt>
2013-06-01 20:45:42 +0100

    1110687 : Script updated to support new language see #3

--- scripts/cc.bat
+++ scripts/cc.bat
@@ -1,2 +1,3 @@
 @java -cp ../lib/antlr.jar antlr.Tool -o ../src/csheets/core/formula/compiler ../src/csheets/core/formula/compiler/FormulaCompiler.g
 @java -cp ../lib/antlr.jar antlr.Tool -o ../src/csheets/ext/assertion ../src/csheets/ext/assertion/AssertionParser.g
+@java -cp ../lib/antlr.jar antlr.Tool -o ../src/csheets/core/formula/newcompiler ../src/csheets/core/formula/newcompiler/FormulaNewCompiler.g
