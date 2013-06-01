a52f3bfb6572b3506dc572dabc63f00eaf42ae23
Filipe Silva 1110687 <>
2013-06-01 17:56:20 +0100

    1110687 : added new package for the new compiler files (created by the cc script), created new Formula Compiler to acknowledge the new formula system, see #3

---
+++ src/csheets/core/formula/newcompiler/FormulaParserTokenTypes.java
@@ -0,0 +1,38 @@
+// $ANTLR 2.7.5 (20050128): "../src/csheets/core/formula/newcompiler/FormulaNewCompiler.g" -> "FormulaLexer.java"$
+package csheets.core.formula.newcompiler;
+public interface FormulaParserTokenTypes {
+	int EOF = 1;
+	int NULL_TREE_LOOKAHEAD = 3;
+	int HASHTAG = 4;
+	int CELL_REF = 5;
+	int ATR = 6;
+	int EQ = 7;
+	int NEQ = 8;
+	int GT = 9;
+	int LT = 10;
+	int LTEQ = 11;
+	int GTEQ = 12;
+	int AMP = 13;
+	int PLUS = 14;
+	int MINUS = 15;
+	int MULTI = 16;
+	int DIV = 17;
+	int POWER = 18;
+	int PERCENT = 19;
+	int LPAR = 20;
+	int RPAR = 21;
+	int FUNCTION = 22;
+	int SEMI = 23;
+	int COLON = 24;
+	int NAME = 25;
+	int NUMBER = 26;
+	int STRING = 27;
+	int LETTER = 28;
+	int ALPHABETICAL = 29;
+	int QUOT = 30;
+	int DIGIT = 31;
+	int ABS = 32;
+	int EXCL = 33;
+	int COMMA = 34;
+	int WS = 35;
+}
