54a5dce25eecd242bdc6ffd7ac89ab5c14196e71
Gil Castro <1110484@isep.ipp.pt>
2013-06-02 02:20:24 +0100

    Added missing libraries required by track 1 to Linux and Mac test script.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp ../tmp-build-tests:../dist/csheets.jar:../lib/antlr.jar:../lib/junit-4.10.jar org.junit.runner.JUnitCore csheets.core.WorkbookTest csheets.core.SpreadsheetTest csheets.ext.comments.CommentableCellTest csheets.ext.db.ApplicationLayerTests csheets.core.formula.lang.AttributionTest csheets.ext.oi.XMLLoadTests csheets.ext.oi.XMLSaveTests
+java -cp ../tmp-build-tests:../dist/csheets.jar:../lib/antlr.jar:../lib/h2-1.3.172.jar:../lib/hsqldb-2.2.9.jar:../lib/junit-4.10.jar org.junit.runner.JUnitCore csheets.core.WorkbookTest csheets.core.SpreadsheetTest csheets.ext.comments.CommentableCellTest csheets.ext.db.ApplicationLayerTests csheets.core.formula.lang.AttributionTest csheets.ext.oi.XMLLoadTests csheets.ext.oi.XMLSaveTests
 
