f1b0f960cc7b262947d8c90a264f90722a6ab466
Gil Castro <1110484@isep.ipp.pt>
2013-06-02 01:53:22 +0100

    Changed run_tests.sh and cc.sh in order to execute our tests.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp ../tmp-build-tests:../dist/csheets.jar:../lib/antlr.jar:../lib/junit-4.10.jar org.junit.runner.JUnitCore csheets.core.WorkbookTest csheets.core.SpreadsheetTest csheets.ext.comments.CommentableCellTest csheets.ext.db.ApplicationLayerTests csheets.core.formula.lang csheets.ext.oi.XMLLoadTests csheets.ext.oi.XMLSaveTests
+java -cp ../tmp-build-tests:../dist/csheets.jar:../lib/antlr.jar:../lib/junit-4.10.jar org.junit.runner.JUnitCore csheets.core.WorkbookTest csheets.core.SpreadsheetTest csheets.ext.comments.CommentableCellTest csheets.ext.db.ApplicationLayerTests csheets.core.formula.lang.AttributionTest csheets.ext.oi.XMLLoadTests csheets.ext.oi.XMLSaveTests
 
