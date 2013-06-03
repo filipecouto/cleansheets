bb814159d9f3646ea16e5508bea9bc694ba85d7d
Gil Castro <1110484@isep.ipp.pt>
2013-06-03 23:24:31 +0100

    Initial work on track 3 (see #7): multiple expressions support in formulas (it looks like the other files failed to be sent)

---
+++ src/csheets/core/formula/lang/ExpressionSet.java
@@ -0,0 +1,52 @@
+package csheets.core.formula.lang;
+
+import java.util.ArrayList;
+
+import csheets.core.IllegalValueTypeException;
+import csheets.core.Value;
+import csheets.core.formula.Expression;
+import csheets.core.formula.util.ExpressionVisitor;
+
+/**
+ * This class, implementing Expression, is a collection of Expressions and can
+ * be used to execute many expressions in one.
+ * 
+ * @author Gil Castro (gil_1110484)
+ */
+public class ExpressionSet implements Expression {
+	private static final long serialVersionUID = 6419944190293825722L;
+
+	private ArrayList<Expression> expressions = new ArrayList<Expression>();
+
+	/**
+	 * Adds the expression to this collection in order to be used with evaluate()
+	 * and accept()
+	 * 
+	 * @param expression
+	 *           the Expression to add
+	 */
+	public void addExpression(Expression expression) {
+		if (expression != null) {
+			expressions.add(expression);
+		}
+	}
+
+	@Override
+	public Value evaluate() throws IllegalValueTypeException {
+		Value value = null;
+		for (Expression e : expressions) {
+			value = e.evaluate();
+		}
+		return value;
+	}
+
+	@Override
+	public Object accept(ExpressionVisitor visitor) {
+		Object result = null;
+		for (Expression e : expressions) {
+			result = e.accept(visitor);
+		}
+		return result;
+	}
+
+}
