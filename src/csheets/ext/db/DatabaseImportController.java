b31e503a5fe6bc6d1ede782165ff5bdbc65e0d11
PIPOKID <PIPOKID@PIPOKID-DESKTOP>
2013-06-06 00:40:39 +0100

    1110687 : working on method importM see #5

--- src/csheets/ext/db/DatabaseImportController.java
+++ src/csheets/ext/db/DatabaseImportController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import csheets.core.Cell;
 import csheets.core.Spreadsheet;
+import csheets.core.formula.compiler.FormulaCompilationException;
 
 /*
  * 
@@ -55,6 +56,28 @@ public class DatabaseImportController {
     
     public void importM() {
         driver.openDatabase(database);
+        String [][] info = driver.getData(tableName);
+        int i=0,j=0;
+        System.out.println("Controller --> Linhas = "+info.length+" Colunas = "+info[0].length);
+        int cellCol,cellRow;
+        cellCol=cell.getAddress().getColumn();
+        cellRow=cell.getAddress().getRow();
+        try{
+            for(i=0;i<info.length;i++){
+                for(j=0;j<info[0].length;j++){
+                    if(i==0){
+                        table.getCell(cellCol+j, cellRow+i).setContent(info[i][j].toString());
+                        System.out.println(info[i][j]);
+                    }else{
+                        table.getCell(cellCol+j, cellRow+i).setContent(info[i][j].substring(1, info[i][j].length()-1).toString());
+                        System.out.println(info[i][j].substring(1, info[i][j].length()-1));
+                    }
+                }
+            }
+        }catch(FormulaCompilationException e){
+            System.out.println(e);
+        }
+        driver.closeDatabase();
     }
     
     public List<String> getTables() {
