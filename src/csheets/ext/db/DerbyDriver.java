03ac9a25bf16eda4ea890faf6e198b25527a3337
Filipe Silva 1110687 <>
2013-06-05 14:09:09 +0100

    1110687 : getTables updated see #5

---
+++ src/csheets/ext/db/DerbyDriver.java
@@ -0,0 +1,121 @@
+package csheets.ext.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class responsible for working with Derby databases
+ * 
+ * @author Filipe Silva
+ */
+public class DerbyDriver implements DatabaseInterface{
+    
+    private Connection databaseConnection;
+
+    @Override
+    public boolean createTable(String name, String[] columns) {
+        try {
+	    String Statement = "CREATE TABLE " + name + "(";
+	    for (int i = 0; i < columns.length; i++) {
+		Statement += DatabaseExportHelper.PrepareColumnName(columns[i],i) + " varchar(512)";
+		if ((i + 1) != columns.length) {
+		    Statement += ",";
+		}
+	    }
+	    Statement += ")";
+	    databaseConnection.prepareStatement(Statement).execute();
+	} catch (SQLException e) {
+	    if(e.getMessage().contains("object name already exists")) {
+		throw new RuntimeException("Table name already exists");
+	    }
+	    e.printStackTrace();
+	    return false;
+	}
+	return true;
+    }
+
+    @Override
+    public boolean addLine(String table, String[] values) {
+        try {
+	    PreparedStatement preparedStatement;
+	    String Statement = "INSERT INTO " + table + " VALUES(";
+	    for (int i = 0; i < values.length; i++) {
+		Statement += "?";
+		if ((i + 1) != values.length) {
+		    Statement += ",";
+		}
+	    }
+	    Statement += ")";
+	    preparedStatement = databaseConnection.prepareStatement(Statement);
+	    for (int i = 1; i <= values.length; i++) {
+		preparedStatement.setString(i, values[i-1]);
+	    }
+	    preparedStatement.execute();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	    return false;
+	}
+	return true;
+    }
+
+    @Override
+    public void openDatabase(String database) {
+        try {
+	    Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+	    databaseConnection = DriverManager.getConnection("jdbc:derby:"
+		    + database);
+	} catch (ClassNotFoundException e) {
+	    e.printStackTrace();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    @Override
+    public void closeDatabase() {
+        try {
+	    databaseConnection.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    @Override
+    public String getName() {
+        return "Derby";
+    }
+
+    @Override
+    public boolean requiresUsername() {
+        return false;
+    }
+
+    @Override
+    public boolean requiresPassword() {
+        return false;
+    }
+
+    @Override
+    public List<String> getTables() {
+        List<String> tables = new ArrayList<String>();
+        try{
+            ResultSet rs = databaseConnection.getMetaData().getTables(null, "PUBLIC", "%",
+		    null);
+            while (rs.next()) {
+                tables.add(rs.getString(3));
+                System.out.println(rs.getString(3));
+            }
+            rs.close();
+            databaseConnection.close();
+        } catch(SQLException e){
+            System.out.println(e);
+        }
+        return tables;
+    }
+    
+}
