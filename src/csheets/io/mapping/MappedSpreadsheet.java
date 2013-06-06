a76fcc4d27f02e3fe048fbc8ab5bc095ecd6fae6
Gil Castro <1110484@isep.ipp.pt>
2013-06-06 22:48:47 +0100

    MappedSpreadsheet documentation

--- src/csheets/io/mapping/MappedSpreadsheet.java
+++ src/csheets/io/mapping/MappedSpreadsheet.java
@@ -11,6 +11,12 @@ import javax.persistence.OneToMany;
 import csheets.core.Cell;
 import csheets.core.Spreadsheet;
 
+/**
+ * This class holds all the data needed to store a Spreadsheet in the database
+ * and also builds a new one from its contained data
+ * 
+ * @author Gil Castro (gil_1110484)
+ */
 @Entity(name = "Spreadsheet")
 class MappedSpreadsheet {
 	@Id
@@ -24,10 +30,24 @@ class MappedSpreadsheet {
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "spreadsheet")
 	private List<MappedCell> cells;
 
+	/**
+	 * Constructor needed to rebuild the data from a persistent source
+	 */
 	MappedSpreadsheet() {
 		// empty
 	}
 
+	/**
+	 * Constructor that stores all the needed data.
+	 * 
+	 * This constructor should be used when the user wishes to save his/her
+	 * workbook and then pass this instance to the persistence target.
+	 * 
+	 * @param workbook
+	 *           the parent of this Spreadsheet representation
+	 * @param sheet
+	 *           the Spreadsheet to store
+	 */
 	MappedSpreadsheet(MappedWorkbook workbook, Spreadsheet sheet) {
 		this.workbook = workbook;
 
@@ -38,6 +58,14 @@ class MappedSpreadsheet {
 		}
 	}
 
+	/**
+	 * Restores all the stored data into this <code>sheet</code>.
+	 * 
+	 * This should be used when the user wishes to open his/her workbook.
+	 * 
+	 * @param sheet
+	 *           the Spreadsheet where to restore this data into
+	 */
 	void makeSpreadsheet(Spreadsheet sheet) {
 		sheet.setTitle(title);
 
