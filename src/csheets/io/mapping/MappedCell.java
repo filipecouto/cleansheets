31d4a6410d61c6273aee918c33dd8cc01fb8506a
Gil Castro <1110484@isep.ipp.pt>
2013-06-06 21:37:02 +0100

    Some work on three classes in charge of containing and building those classes the user wants to save in the file (see #6)

--- src/csheets/io/mapping/MappedCell.java
+++ src/csheets/io/mapping/MappedCell.java
@@ -10,6 +10,10 @@ import javax.persistence.ManyToOne;
 import javax.swing.border.Border;
 
 import csheets.core.Address;
+import csheets.core.Cell;
+import csheets.core.formula.compiler.FormulaCompilationException;
+import csheets.ext.style.StylableCell;
+import csheets.ext.style.StyleExtension;
 
 @Entity(name = "Cell")
 public class MappedCell {
@@ -29,4 +33,50 @@ public class MappedCell {
 	private Color foregroundColor;
 	private int horizontalAlignment;
 	private int verticalAlignment;
+
+	private MappedCell() {
+		// empty
+	}
+
+	MappedCell(MappedSpreadsheet spreadsheet, Cell cell) {
+		this.spreadsheet = spreadsheet;
+
+		address = cell.getAddress();
+		content = cell.getContent();
+
+		final Cell extension = cell.getExtension(StyleExtension.NAME);
+		if (extension != null) {
+			final StylableCell style = (StylableCell) extension;
+			backgroundColor = style.getBackgroundColor();
+			border = style.getBorder();
+			font = style.getFont();
+			foregroundColor = style.getForegroundColor();
+			horizontalAlignment = style.getHorizontalAlignment();
+			verticalAlignment = style.getVerticalAlignment();
+		}
+	}
+
+	void makeCell(Cell cell) {
+		try {
+			cell.setContent(content);
+		} catch (FormulaCompilationException e) {
+			// we shouldn't keep the user from seeing his/her file just because
+			// there's a wrong formula
+		}
+
+		final Cell extension = cell.getExtension(StyleExtension.NAME);
+		if (extension != null) {
+			final StylableCell style = (StylableCell) extension;
+			style.setBackgroundColor(backgroundColor);
+			style.setBorder(border);
+			style.setFont(font);
+			style.setForegroundColor(foregroundColor);
+			style.setHorizontalAlignment(horizontalAlignment);
+			style.setVerticalAlignment(verticalAlignment);
+		}
+	}
+	
+	Address getAddress() {
+		return address;
+	}
 }
--- src/csheets/io/mapping/MappedSpreadsheet.java
+++ src/csheets/io/mapping/MappedSpreadsheet.java
@@ -1,14 +1,46 @@
 package csheets.io.mapping;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import csheets.core.Cell;
+import csheets.core.Spreadsheet;
 
 @Entity(name = "Spreadsheet")
-public class MappedSpreadsheet {
+class MappedSpreadsheet {
 	@Id
 	private int id;
-	
+
 	private MappedWorkbook workbook;
-	
-	private String name;
+
+	private String title;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "spreadsheet")
+	private List<MappedCell> cells;
+
+	private MappedSpreadsheet() {
+		// empty
+	}
+
+	MappedSpreadsheet(MappedWorkbook workbook, Spreadsheet sheet) {
+		this.workbook = workbook;
+
+		title = sheet.getTitle();
+
+		for (Cell cell : sheet) {
+			cells.add(new MappedCell(this, cell));
+		}
+	}
+
+	void makeSpreadsheet(Spreadsheet sheet) {
+		sheet.setTitle(title);
+
+		for (MappedCell cell : cells) {
+			cell.makeCell(sheet.getCell(cell.getAddress()));
+		}
+	}
 }
