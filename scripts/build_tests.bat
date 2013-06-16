ebd08310f10c98d6af40d7d56e0439b8de97e159
noobshark <FILIPE@FILIPE-PC>
2013-06-17 00:37:03 +0100

    Scripts are now updated to support the tests.

--- scripts/build_tests.bat
+++ scripts/build_tests.bat
@@ -3,5 +3,5 @@ rmdir /S /Q  ..\tmp-build-tests
 mkdir ..\tmp-build-tests
 REM del /S /Q ..\tmp-build-tests\*.class >nul
 dir /B /S /O:N ..\src-tests\*.java > c.lst
-javac -cp ..\src-tests;..\lib\antlr.jar;..\dist\csheets.jar;..\lib\junit-4.10.jar -d ..\tmp-build-tests @c.lst %1 %2 %3
+javac -cp ..\src-tests\;..\lib\*;..\dist\*; -d ..\tmp-build-tests @c.lst %1 %2 %3
 del c.lst
--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,2 +1,2 @@
-@java -cp ..\tmp-build-tests;..\dist\csheets.jar;..\lib\antlr.jar;..\lib\junit-4.10.jar org.junit.runner.JUnitCore csheets.core.WorkbookTest csheets.core.SpreadsheetTest csheets.ext.comments.CommentableCellTest csheets.ext.db.ApplicationLayerTests csheets.core.formula.lang.AttributionTest csheets.ext.oi.XMLLoadTests csheets.ext.oi.XMLSaveTests
+@java -cp ..\tmp-build-tests;..\dist\*;..\lib\*;..\dist\.; org.junit.runner.JUnitCore csheets.core.WorkbookTest csheets.core.SpreadsheetTest csheets.ext.comments.CommentableCellTest csheets.ext.db.ApplicationLayerTests csheets.core.formula.lang.AttributionTest csheets.ext.io.XMLDBUnitTest csheets.core.formula.lang.MultipleExpressionsTest csheets.ext.versioning.VersionManagementTest
 
